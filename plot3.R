7b1d84291c8a363bd8451fe3473eb8604fb4cea5
student <scarsdale@users.noreply.github.com>
2014-05-11 19:08:23 +0900

    add function for plot 3 (line chart of 3 submetering variables)

---
+++ plot3.R
@@ -0,0 +1,28 @@
+if (!exists("householdpower")) {
+  source("getdata.R")
+}
+if (!exists("make.png")) {
+  source("makepng.R")
+}
+## a line graph of submetering (3 variables) from 1 Feb to 2 Feb 2007
+plot3 <- function() {
+  dat <- householdpower$dates("20070201", "20070203")
+  submetering <- c("Sub_metering_1",
+                   "Sub_metering_2",
+                   "Sub_metering_3")
+  colors = c("black", "red", "blue")
+  plot(dat$datetime,
+       dat[[submetering[1]]],
+       ylab="Energy sub metering",
+       xlab="",      
+       main="",
+       type="l",
+       col=colors[1]
+       )
+  lines(dat$datetime, dat[[submetering[2]]], col=colors[2])
+  lines(dat$datetime, dat[[submetering[3]]], col=colors[3])
+  legend("topright", submetering, col=colors, lty=rep(1, 3))
+}
+plot3png <- function() {
+  make.png("plot3", plot3)
+}
