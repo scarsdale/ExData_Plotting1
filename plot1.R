8a217e90c24311450fb178214ef7a92215b33126
student <scarsdale@users.noreply.github.com>
2014-05-11 18:44:39 +0900

    make sourcing of other scripts conditional

--- plot1.R
+++ plot1.R
@@ -1,5 +1,9 @@
-source("getdata.R")
-source("makepng.R")
+if (!exists("householdpower")) {
+  source("getdata.R")
+}
+if (!exists("make.png")) {
+  source("makepng.R")
+}
 ## a histogram of global active power from 1 Feb to 2 Feb 2007
 plot1 <- function() {
   dat <- householdpower$dates("20070201", "20070203")
