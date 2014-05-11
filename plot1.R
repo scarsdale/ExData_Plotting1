ffe20e4a6a13becf3da1fe2e7d04ba55a6bdf538
student <scarsdale@users.noreply.github.com>
2014-05-11 18:41:29 +0900

    widen date range to actually get two days of data

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@ source("getdata.R")
 source("makepng.R")
 ## a histogram of global active power from 1 Feb to 2 Feb 2007
 plot1 <- function() {
-  dat <- householdpower$dates("20070201", "20070202")
+  dat <- householdpower$dates("20070201", "20070203")
   hist(dat[["Global_active_power"]],
        xlab="Global Active Power (kilowatts)",
        main="Global Active Power",
